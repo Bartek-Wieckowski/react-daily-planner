20b1268a15c6166a05521bc746f7dd7abbffaa2c
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-08 20:55:20 +0200

    added DailyPlannerProvider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { DailyPlannerProvider } from './context/DailyPlannerContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DailyPlannerProvider>
+      <App />
+    </DailyPlannerProvider>
   </React.StrictMode>
 );
