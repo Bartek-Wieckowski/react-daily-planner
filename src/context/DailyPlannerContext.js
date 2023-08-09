0dd75a2f852aafa48085ca318629b6a66303c713
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-09 16:10:47 +0200

    created variable array objects items, assigned this array to key in initialState, used hook to save localStorage and passed to Provider value

--- src/context/DailyPlannerContext.js
+++ src/context/DailyPlannerContext.js
@@ -1,9 +1,54 @@
 import { createContext, useContext, useReducer } from 'react';
+import useLocalStorageState from '../hooks/useLocalStorageState';
+
+const MY_DAILY_TODOLIST = [
+  {
+    id: crypto.randomUUID(),
+    status: false,
+    body: 'Morning learning - 1h',
+  },
+  {
+    id: crypto.randomUUID(),
+    status: false,
+    body: 'Workout',
+  },
+  {
+    id: crypto.randomUUID(),
+    status: false,
+    body: 'Cold shower',
+  },
+  {
+    id: crypto.randomUUID(),
+    status: false,
+    body: 'Work - 8h',
+  },
+  {
+    id: crypto.randomUUID(),
+    status: false,
+    body: 'English learning - 30min',
+  },
+  {
+    id: crypto.randomUUID(),
+    status: false,
+    body: 'Family time',
+  },
+  {
+    id: crypto.randomUUID(),
+    status: false,
+    body: 'Evening learning - 1h',
+  },
+  {
+    id: crypto.randomUUID(),
+    status: false,
+    body: 'Stretching and breath work!',
+  },
+];
 
 const DailyPlannerContext = createContext();
 
 const initialState = {
   isDarkMode: false,
+  myDailyTodoList: MY_DAILY_TODOLIST,
 };
 
 function reducer(state, action) {
@@ -17,14 +62,20 @@ function reducer(state, action) {
 }
 
 function DailyPlannerProvider({ children }) {
-  const [{ isDarkMode }, dispatch] = useReducer(reducer, initialState);
+  const [{ isDarkMode, myDailyTodoList }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  const [myTodos, setMyTodos] = useLocalStorageState(myDailyTodoList, 'todo');
 
   function handleDarkMode() {
     dispatch({ type: 'onDarkMode' });
   }
 
   return (
-    <DailyPlannerContext.Provider value={{ isDarkMode, handleDarkMode }}>
+    <DailyPlannerContext.Provider
+      value={{ isDarkMode, handleDarkMode, myTodos }}
+    >
       {children}
     </DailyPlannerContext.Provider>
   );
