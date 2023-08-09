1a37cc1b4d2d4494620f2048bb397f85fd43df2f
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-09 20:32:46 +0200

    added few html tag for test and display item body

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,4 +1,14 @@
 import './item.css';
 export default function Item({ todo }) {
-  return <li></li>;
+  return (
+    <li className="item-wrapper">
+      <div className="item-status-text">
+        <span>{todo.body}</span>
+      </div>
+      <div className="item-actions">
+        <button className="bg-info">EDIT</button>
+        <button className="bg-danger">DELETE</button>
+      </div>
+    </li>
+  );
 }
