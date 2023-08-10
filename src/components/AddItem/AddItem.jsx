480ae806458bf99a3ffa24ea333395cfd77ce97d
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-10 19:47:03 +0200

    created AddItem component: used createdItem function, maked handleSubmit which allow to add new item

---
+++ src/components/AddItem/AddItem.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { useDailyPlanner } from '../../context/DailyPlannerContext';
+import './add-item.css';
+
+export default function AddItem() {
+  const { createdItem } = useDailyPlanner();
+  const [body, setBody] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!body) {
+      console.log('error- dodac notify popup');
+      return;
+    }
+
+    const newItem = {
+      id: crypto.randomUUID(),
+      status: false,
+      body,
+    };
+
+    createdItem(newItem);
+    setBody('');
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-control">
+        <input
+          type="text"
+          className="form-input"
+          placeholder="write smoething"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <button className="btn bg-success text-white" onClick={handleSubmit}>
+          Add
+        </button>
+      </div>
+    </form>
+  );
+}
