76b07a3667c439e53633ab989bece13d89f664fd
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-10 20:23:56 +0200

    created ability to change status

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,9 +1,21 @@
+import { useDailyPlanner } from '../../context/DailyPlannerContext';
 import './item.css';
 export default function Item({ todo }) {
+  const { changeStatusItem } = useDailyPlanner();
+  const checkedTodo = todo.status === true ? 'checked' : '';
   return (
     <li className="item-wrapper">
       <div className="item-status-text">
-        <span>{todo.body}</span>
+        <label class="label-input">
+          <input
+            type="checkbox"
+            value={todo.status}
+            onChange={(e) => changeStatusItem(todo.id)}
+            checked={checkedTodo}
+          />
+          {!checkedTodo ? <span>{todo.body}</span> : <del>{todo.body}</del>}
+          <span class="checkmark"></span>
+        </label>
       </div>
       <div className="item-actions">
         <button className="btn text-white bg-info">EDIT</button>
