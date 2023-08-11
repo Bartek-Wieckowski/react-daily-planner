a321c23be60ed9c713000a791056cbd56b426275
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-11 21:03:12 +0200

    created derived state based on it display some text information depending on the value

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -3,12 +3,23 @@ import './progress-bar.css';
 
 export default function ProgressBar() {
   const { myDailyTodoList, countCompletedTodoItem } = useDailyPlanner();
+  const allTodosLength = myDailyTodoList.length;
   return (
     <>
-      <progress
-        max={myDailyTodoList.length}
-        value={countCompletedTodoItem}
-      ></progress>
+      <progress max={allTodosLength} value={countCompletedTodoItem} />
+      <span>
+        {allTodosLength === 0 && 'Create your first task 1️⃣'}
+        {allTodosLength > 0 &&
+          countCompletedTodoItem === 0 &&
+          'Go on! complete the first task 🏃‍♂️'}
+        {allTodosLength > 0 &&
+          countCompletedTodoItem >= 1 &&
+          countCompletedTodoItem < allTodosLength &&
+          'Go on! complete the next task 😉'}
+        {allTodosLength > 0 &&
+          allTodosLength === countCompletedTodoItem &&
+          'Yeah! You got all tasks! 🎉'}
+      </span>
     </>
   );
 }
