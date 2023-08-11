2c3b935d19b07517c5fd895fbed4015742bd43ca
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-11 20:27:53 +0200

    created two state helpers, added two functions, used function from context, and change jsx to better display design

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,24 +1,56 @@
+import { useState } from 'react';
 import { useDailyPlanner } from '../../context/DailyPlannerContext';
 import './item.css';
+
 export default function Item({ todo }) {
-  const { changeStatusItem } = useDailyPlanner();
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingBody, setEditingBody] = useState(todo.body);
+  const { changeStatusItem, editedItem } = useDailyPlanner();
   const checkedTodo = todo.status === true ? 'checked' : '';
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+  function handleSaveClick() {
+    editedItem(todo.id, editingBody);
+    setIsEditing(false);
+  }
+
   return (
     <li className="item-wrapper">
       <div className="item-status-text">
-        <label class="label-input">
+        {isEditing ? (
           <input
-            type="checkbox"
-            value={todo.status}
-            onChange={(e) => changeStatusItem(todo.id)}
-            checked={checkedTodo}
+            type="text"
+            value={editingBody}
+            onChange={(e) => setEditingBody(e.target.value)}
           />
-          {!checkedTodo ? <span>{todo.body}</span> : <del>{todo.body}</del>}
-          <span class="checkmark"></span>
-        </label>
+        ) : (
+          <label class="label-input">
+            <input
+              type="checkbox"
+              value={todo.status}
+              onChange={(e) => changeStatusItem(todo.id)}
+              checked={checkedTodo}
+            />
+            {!checkedTodo ? <span>{todo.body}</span> : <del>{todo.body}</del>}
+            <span class="checkmark"></span>
+          </label>
+        )}
       </div>
       <div className="item-actions">
-        <button className="btn text-white bg-info">EDIT</button>
+        {isEditing ? (
+          <button
+            className="btn text-white bg-primary"
+            onClick={handleSaveClick}
+          >
+            SAVE
+          </button>
+        ) : (
+          <button className="btn text-white bg-info" onClick={handleEditClick}>
+            EDIT
+          </button>
+        )}
+
         <button className="btn text-white bg-danger">DELETE</button>
       </div>
     </li>
