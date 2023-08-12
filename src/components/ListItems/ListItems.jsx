e0719dbf2739805ea67a83a70e41b17cc57766a2
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-12 13:49:06 +0200

    added special var to maked sort list items, and logic, change jsx

--- src/components/ListItems/ListItems.jsx
+++ src/components/ListItems/ListItems.jsx
@@ -3,12 +3,38 @@ import Item from '../Item/Item';
 import './list-items.css';
 
 export default function ListItems() {
-  const { myDailyTodoList: myTodos } = useDailyPlanner();
+  const { myDailyTodoList, sortingOptions, sortedItems } = useDailyPlanner();
+
+  let sortedList = [...myDailyTodoList];
+
+  function handleSortChange(e) {
+    sortedItems(e.target.value);
+  }
+
+  if (sortingOptions === 'order') {
+    sortedList = myDailyTodoList;
+  }
+  if (sortingOptions === 'status') {
+    sortedList.sort((a, b) => Number(b.status) - Number(a.status));
+  }
+  if (sortingOptions === 'alphabetically') {
+    sortedList.sort((a, b) => a.body.localeCompare(b.body));
+  }
+
   return (
-    <ul className="list-items">
-      {myTodos.map((item) => (
-        <Item todo={item} key={item.id} />
-      ))}
-    </ul>
+    <>
+      <div className="filtered-actions">
+        <select value={sortingOptions} onChange={handleSortChange}>
+          <option value="order">Sort by order</option>
+          <option value="status">Sort by status</option>
+          <option value="alphabetically">Sort alphabetically</option>
+        </select>
+      </div>
+      <ul className="list-items">
+        {sortedList.map((item) => (
+          <Item todo={item} key={item.id} />
+        ))}
+      </ul>
+    </>
   );
 }
